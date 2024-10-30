477a865223341438dda37ec6848bfd31fed05841
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-10-30 16:38:16 -0500

    practicing binary search

---
+++ Binary_Search/binary_search.py
@@ -0,0 +1,20 @@
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        middle = (low + high) // 2
+        if arr[middle] == target:
+            return middle
+        elif arr[middle] > target:
+            high = middle
+        elif arr[middle] < target:
+            low = middle
+    return -1
+
+
+numbers = [2,3,5,6,7,8,23,45,65,101]
+target = 8
+
+print(binary_search(numbers, target))
