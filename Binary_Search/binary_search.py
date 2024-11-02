9161b343875be312d9fadafe37fcd99df6c19f1a
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-11-02 00:08:53 -0500

    Update binary_search.py

--- Binary_Search/binary_search.py
+++ Binary_Search/binary_search.py
@@ -28,8 +28,23 @@ def search(nums, target):
             high = middle - 1
     return -1
 
+def binary_search2(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        middle = int((low + high) // 2)
+        if arr[middle] == target:
+            return middle
+        elif arr[middle] > target:
+            high = middle - 1
+        elif arr[middle] < target:
+            low = middle + 1
+    return -1
+
+
 
 numbers = [2,3,5,6,7,8,23,45,65,101]
-target = 2
+target = 90
 
-print(search(numbers, target))
+print(binary_search2(numbers, target))
