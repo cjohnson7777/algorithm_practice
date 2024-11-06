cd2219e98814f7cbe901f873dac7b338ee2a6257
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-11-05 22:55:12 -0600

    has_path question

--- Graphs/depth_first_search.py
+++ Graphs/depth_first_search.py
@@ -17,13 +17,31 @@ def iterative_depth_first_print(graph, source):
         for neighbor in graph[current]:
             stack.append(neighbor)
 
-#iterative_depth_first_print(graph, 'a')
-
 def recursive_depth_first_print(graph, source):
     print(source)
 
     for neighbor in graph[source]:
         recursive_depth_first_print(graph, neighbor)
+
+
+
+def iterative_depth_first_search(graph, source):
+    stack = [source]
+
+    while stack:
+        current = stack.pop()
+        print(current)
+
+        for neighbor in graph[current]:
+            stack.append(neighbor)
+
+
+def recursive_depth_first_search(graph, source):
+    print(source)
+ 
+    for neighbor in graph[source]:
+        recursive_depth_first_search(graph, neighbor)
+
 
 print('\t Recursive')
 recursive_depth_first_print(graph, 'a')
---
+++ Graphs/has_path.py
@@ -0,0 +1,36 @@
+graph = {
+    'f': ['g', 'i'],
+    'g': ['h'],
+    'h': [],
+    'i': ['g', 'k'],
+    'j': ['i'],
+    'k': [],
+}
+
+#depth first search
+def has_path_depth(graph, src, dst):
+    if src == dst: 
+        return True
+    
+    for neighbor in graph[src]:
+        if has_path_depth(graph, neighbor, dst) == True:
+            return True
+        
+    return False
+
+#breadth first search
+def has_path_breadth(graph, src, dst):
+    queue = [src]
+
+    while queue:
+        current = queue.pop(0)
+        if current == dst:
+            return True
+        for neighbor in graph[current]:
+            queue.append(neighbor)
+    
+    return False
+
+
+#print(has_path_depth(graph, 'f', 'k'))
+print(has_path_breadth(graph, 'f', 'k'))
