1ecd613e80abdb30a0eb2538faaddaea9d9cb8b7
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-11-07 16:28:40 -0600

    more bfs practice

--- Graphs/breadth_first_search.py
+++ Graphs/breadth_first_search.py
@@ -16,5 +16,21 @@ def breadth_first_print(graph, source):
         for neighbor in graph[current]:
             queue.append(neighbor)
 
+
+#breadth first search practice #1
+def breadth_first_search(graph, source):
+    queue = [source]
+
+    while queue:
+        current = queue.pop(0)
+        print(current)
+        for neighbor in graph[current]:
+            queue.append(neighbor)
+
+
+
+
 breadth_first_print(graph, 'a')
+print('\t Practice')
+breadth_first_search(graph, 'a')
     
--- Graphs/has_path.py
+++ Graphs/has_path.py
@@ -31,6 +31,22 @@ def has_path_breadth(graph, src, dst):
     
     return False
 
+#breadth first has_path practice #1
+def has_path_breadth_two(graph, src, dst):
+    queue = [src]
+
+    while queue:
+        current = queue.pop(0)
+        if current == dst:
+            return True
+        
+        for neighbor in graph[current]:
+            queue.append(neighbor)
+    
+    return False
+
+
 
 #print(has_path_depth(graph, 'f', 'k'))
-print(has_path_breadth(graph, 'f', 'k'))
+#print(has_path_breadth(graph, 'f', 'k'))
+print(has_path_breadth_two(graph, 'f', 'k'))
