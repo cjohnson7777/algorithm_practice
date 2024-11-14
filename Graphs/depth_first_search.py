789fbc178b48111d977bdc36bc2ba00a94cafb6d
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-11-13 22:56:59 -0600

    Update depth_first_search.py

--- Graphs/depth_first_search.py
+++ Graphs/depth_first_search.py
@@ -43,7 +43,14 @@ def recursive_depth_first_search(graph, source):
         recursive_depth_first_search(graph, neighbor)
 
 
+def recursive_depth_first_search_practice_one(graph, source):
+    print(source)
+    for neighbor in graph[source]:
+        recursive_depth_first_search_practice_one(graph, neighbor)
+
 print('\t Recursive')
-recursive_depth_first_print(graph, 'a')
+#recursive_depth_first_print(graph, 'a')
 print('\t Iterative')
-iterative_depth_first_print(graph, 'a')
+#iterative_depth_first_print(graph, 'a')
+#recursive_depth_first_search_practice_one(graph, 'a')
+print(graph)
