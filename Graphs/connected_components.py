7310bf22c0f66aae5678f33a67a1e61d242e5c84
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-12-27 22:32:02 -0600

    Update connected_components.py

--- Graphs/connected_components.py
+++ Graphs/connected_components.py
@@ -31,7 +31,8 @@ def explore(graph, current, visited):
     
     return True
 
-print(connected_components_count(graph))
+
+print('Correct: ',connected_components_count(graph))
 
 
 def connected_components_practice_2(graph):
@@ -72,8 +73,33 @@ def explore_practice_3(graph, current, visited):
     
     visited.add(current)
 
-    for neighbor in graph:
+    for neighbor in graph[current]:
         explore_practice_3(graph, neighbor, visited)
     
     return True
     
+
+def connected_components_count_4(graph):
+    visited = set()
+    count = 0
+
+    for node in graph:
+        if explore_practice_4(graph, node, visited) == True:
+            count += 1
+    
+    return count
+
+
+def explore_practice_4(graph, current, visited):
+    if current in visited:
+        return False
+    
+    visited.add(current)
+
+    for neighbor in graph[current]:
+        explore_practice_4(graph, neighbor, visited)
+    
+    return True
+
+
+print(connected_components_count_4(graph))
