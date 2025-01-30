0ee178acd7fdc096d4368b38d33f449bcc4452d9
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2025-01-30 01:43:18 -0600

    Update fib_memoization.py

--- Dynamic_Programming/fib_memoization.py
+++ Dynamic_Programming/fib_memoization.py
@@ -1,7 +1,19 @@
-def fib(n):
+#original fibonacchi
+def fib1(n):
+    if n <= 2:
+        return 1
+    return fib1(n - 1) + fib1(n - 2)
+
+print(fib1(6))
+
+#memoized fibonacci
+def fib(n, memo = {}):
+    if n in memo:
+        return memo[n]
     if n <= 2:
         return 1
     
-    return fib(n - 1) + fib(n - 2)
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+    return memo[n]
 
 print(fib(6))
