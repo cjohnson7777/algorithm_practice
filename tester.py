5d8875f2217b05822825ca3c2eb4512ae13a5759
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2025-01-30 23:30:31 -0600

    Update tester.py

--- tester.py
+++ tester.py
@@ -3,4 +3,15 @@ def encode(strs):
     return "#".join(strs)
 
 strs = ["neet","code","love","you"]
-print(encode(strs))
+#print(encode(strs))
+
+a = "5"
+
+print(a.isdigit())
+
+arr = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+]
+
