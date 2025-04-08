54a1f87f70224e897c3c7db1734f02945df7994c
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2025-04-07 21:24:53 -0500

    solved 40 and 41

--- Daily_Coding_Problem/problem_40.py
+++ Daily_Coding_Problem/problem_40.py
@@ -3,3 +3,19 @@
 # For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given [13, 19, 13, 13], return 19.
 
 # Do this in O(N) time and O(1) space.
+# NOT UNDERSTOOD
+
+def appears_once(nums):
+    ones = 0
+    twos = 0
+
+    for num in nums:
+        ones = (ones ^ num) & ~twos
+        twos = (twos ^ num) & ~ones
+
+    return ones
+
+nums = [6, 1, 3, 3, 3, 6, 6]
+
+print(appears_once(nums))
+print(appears_once([13, 19, 13, 13]))
--- Daily_Coding_Problem/problem_41.py
+++ Daily_Coding_Problem/problem_41.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+import heapq
 # Given an unordered list of flights taken by someone, each represented as (origin, destination) pairs, and a starting airport, compute the person's itinerary. If no such itinerary exists, return null. If there are multiple possible itineraries, return the lexicographically smallest one. All flights must be used in the itinerary.
 
 # For example, given the list of flights [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')] and starting airport 'YUL', you should return the list ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD'].
@@ -6,3 +8,32 @@
 
 # Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and starting airport 'A', you should return the list ['A', 'B', 'C', 'A', 'C'] even though ['A', 'C', 'A', 'B', 'C'] is also a valid itinerary. However, the first one is lexicographically smaller.
 
+
+
+
+def make_itinerary(flights, start):
+    graph = defaultdict(list)
+    itinerary = []
+
+    for origin, dest in flights:
+        heapq.heappush(graph[origin], dest)
+
+    visit(start)
+    itinerary.reverse()
+
+    if len(itinerary) == len(flights) + 1:
+        return itinerary
+    else:
+        return None
+
+
+def visit(airport, graph, itinerary):
+    while graph[airport]:
+        next_stop = heapq.heappop(graph[airport])
+        visit(next_stop)
+    itinerary.append(airport)
+
+
+flights = [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')]
+start = 'YUL'
+print(make_itinerary(flights, start))
