cd065d1ce0316246e8dce06ed98812a28a0651f9
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2025-04-08 22:08:56 -0500

    solved 62, 63, 64

--- Daily_Coding_Problem/problem_62.py
+++ Daily_Coding_Problem/problem_62.py
@@ -5,3 +5,20 @@
 # Right, then down
 # Down, then right
 # Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
+
+
+def grid_traveler(m, n, memo={}):
+    if (m, n) in memo:
+        return memo[(m, n)]
+
+    if m == 0 or n == 0:
+        return 0
+    
+    if m == 1 and n == 1:
+        return 1
+    
+    memo[(m, n)] = grid_traveler(m - 1, n) + grid_traveler(m, n - 1) 
+    
+    return memo[(m, n)]
+
+print(grid_traveler(5, 5))
--- Daily_Coding_Problem/problem_63.py
+++ Daily_Coding_Problem/problem_63.py
@@ -8,3 +8,30 @@
 #  ['M', 'A', 'S', 'S']]
 
 # and the target word 'FOAM', you should return true, since it's the leftmost column. Similarly, given the target word 'MASS', you should return true, since it's the last row.
+
+
+def word_search(matrix, word):
+
+    for r in range(len(matrix)):
+        search = ""
+        for c in range(len(matrix)):
+            search += matrix[r][c]
+            if search == word:
+                return True
+    
+    for r in range(len(matrix)):
+        search = ""
+        for c in range(len(matrix)):
+            search += matrix[c][r]
+            if search == word:
+                return True
+
+
+matrix = [['F', 'A', 'C', 'I'],
+          ['O', 'B', 'Q', 'P'],
+          ['A', 'N', 'O', 'B'],
+          ['M', 'A', 'S', 'S']]
+
+target = 'FOAM'
+
+print(word_search(matrix, target))
